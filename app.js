4e885acef34d5181192857c3e0a78cf912753fd8
khangnm2804 <khangnm2804@gmail.com>
2025-03-16 22:15:25 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the Contact Book Application' });
 });
+
+const contactsRouter = require("./app/routes/contact.route");
+app.use('/api/contacts', contactsRouter);
 module.exports = app;
