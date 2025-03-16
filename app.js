3bd5a6a7721e844e24ffd21cf15b6c1969c89ad9
khangnm2804 <khangnm2804@gmail.com>
2025-03-16 22:23:36 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 
@@ -10,4 +11,14 @@ app.get('/', (req, res) => {
 
 const contactsRouter = require("./app/routes/contact.route");
 app.use('/api/contacts', contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
 module.exports = app;
